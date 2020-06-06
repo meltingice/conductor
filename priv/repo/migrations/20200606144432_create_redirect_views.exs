710a8e6ec9ad6e7759daebf075410924eb9da1cc
Ryan LeFevre <meltingice8917@gmail.com>
2020-06-06 18:06:36 -0400

    Fix redirect_views ID type

--- priv/repo/migrations/20200606144432_create_redirect_views.exs
+++ priv/repo/migrations/20200606144432_create_redirect_views.exs
@@ -2,7 +2,8 @@ defmodule Conductor.Repo.Migrations.CreateRedirectViews do
   use Ecto.Migration
 
   def change do
-    create table(:redirect_views) do
+    create table(:redirect_views, primary_key: false) do
+      add :id, :uuid, primary_key: true
       add :redirect_id, references(:redirects, type: :uuid)
       add :ip_address, :string
       add :user_agent, :string
